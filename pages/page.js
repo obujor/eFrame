f48379522034520f8b77731bce90eccaac988a37
Luca Rinaldi <luca.rinaldi11@studio.unibo.it>
2015-08-20 14:44:35 +0200

    Aggiunto il topic e funziona

--- pages/page.js
+++ pages/page.js
@@ -3,16 +3,17 @@ var context = require('../utils/context.js'),
 
 module.exports = function (req, res, next) {
     var battery = (req.params.battery) ? req.params.battery : 0;
+    var topic = (req.params.topic);
     getNews(function(news) {
         res.render('page', context.create({
             battery: battery,
             news: news
         }));
-    });
+    }, topic);
 };
 
-function getNews(cb) {
-    feed("https://news.google.it/news?cf=all&hl=it&pz=1&ned=it&output=rss", function(err, articles) {
+function getNews(cb,tp) {
+    feed("https://news.google.it/news?cf=all&hl=it&pz=1&topic="+tp+"&ned=it&output=rss", function(err, articles) {
       if (err) throw err;
       cb(articles);
     });
