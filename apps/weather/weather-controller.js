6e8289a5894bff6504af55213a5002abc2863736
Octavian Bujor <ottavio.bo@gmail.com>
2015-09-07 14:55:52 +0200

    Added settings for weather

--- apps/weather/weather-controller.js
+++ apps/weather/weather-controller.js
@@ -2,7 +2,9 @@ var request = require('request'),
     context = require('../../utils/context.js');
 
 exports.getData = function (user, device, cb) {
-    request('http://api.openweathermap.org/data/2.5/weather?units=metric&type=accurate&q='+user.city+'&lang='+user.lang, function (error, response, body) {
+    var city = user.weather && user.weather.city || user.city;
+
+    request('http://api.openweathermap.org/data/2.5/weather?units=metric&type=accurate&q='+city+'&lang='+user.lang, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var data = JSON.parse(body);
             cb(data);
